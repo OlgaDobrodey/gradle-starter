4bee050bb203691ffe4a4c0ec0ebd64023a759c3
OlgaDobrodey <olga.dobrodey@itrexgroup.com>
2022-04-21 10:37:57 +0300

    jap path1

--- src/main/java/com/itrex/HelloWorld.java
+++ src/main/java/com/itrex/HelloWorld.java
@@ -1,7 +1,11 @@
 package com.itrex;
 
+import org.springframework.util.StringUtils;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println(StringUtils.capitalize("hello world"));
     }
 }
+
+
